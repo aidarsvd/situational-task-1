e76aaf5d66c020a3b107cc8b6d488cebbf4aa03f
Aidar <achakiev2003@gmail.com>
2024-07-04 16:25:51 +0600

    initial commit

---
+++ src/main/java/kg/aidar/fin_tech_innovators/FinTechInnovatorsApplication.java
@@ -0,0 +1,13 @@
+package kg.aidar.fin_tech_innovators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTechInnovatorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinTechInnovatorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/aidar/fin_tech_innovators/FinTechInnovatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.aidar.fin_tech_innovators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTechInnovatorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
